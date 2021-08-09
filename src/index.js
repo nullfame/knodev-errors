690f8efe22df8b04a68196d3784786bf244315f2
Adam Finlayson <amf@adamfinlayson.com>
2021-08-08 19:59:26 -0500

    chore: remove: errors that shouldn't export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const errors = require("./errors");
 
 delete errors.ERROR;
+delete errors.formatError;
 delete errors.NAME;
+delete errors.ProjectError;
+delete errors.ProjectMultiError;
 
 module.exports = errors;
